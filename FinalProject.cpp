880680a934cb0307d175954d0e2207a827b1fdbd
Thang Le Quang <10221044@studentvgueduvn.onmicrosoft.com>
2022-12-01 21:55:06 +0700

    create menu interface

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -3,9 +3,23 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
-    cout << "xin chao";
-    cout << "hallo";
-    cout << "vgu";
-    cout << "abc";
+    int options = 0;
+    while (options != 5) {
+        cout << "Hotel room booking management system" << endl;
+        cout << "Menu options:" << endl;
+        cout << "1. Book new room" << endl;
+        cout << "2. Booking records" << endl;
+        cout << "3. Rooms allocated" << endl;
+        cout << "4. Delete record" << endl;
+        cout << "5. Exit" << endl;
+        cout << "Enter option:";
+        cin >> options;
+        switch (options)
+        {
+
+        }
+    }
+
+
+    
 }
